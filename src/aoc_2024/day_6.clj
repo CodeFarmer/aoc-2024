4a28e95a9e1d71f75f1d8885b948f9633230ca4a
Joel <joel@gluth.io>
2024-12-06 21:12:02 +0000

    Brute force solution to day 6 part 2

--- src/aoc_2024/day_6.clj
+++ src/aoc_2024/day_6.clj
@@ -11,32 +11,51 @@
          (not (>= y height))
          (not (< y 0)))))
 
+(defn blocked? [tmap loc]
+  (#{\# \O} (aoc/get-tile tmap loc)))
+
 (defn guard-walk [tmap [x y] dir]
   (let [delta (aoc/directions dir)
         width (aoc/tmap-width tmap)
         height (aoc/tmap-height tmap)]
-    (take-while (fn [[x' y']]
-                  (and (not (> x' width))
-                       (not (< x' -1))
-                       (not (> y' height))
-                       (not (< y' -1))
-                       (not (= \# (aoc/get-tile tmap [x' y'])))))
-                (iterate #(map + delta %) (map + delta [x y])))))
+    (map #(vector % dir)
+         (take-while (fn [[x' y']]
+                       (and (not (> x' width))
+                            (not (< x' -1))
+                            (not (> y' height))
+                            (not (< y' -1))
+                            (not (blocked? tmap [x' y']))))
+                     (iterate #(map + delta %) (map + delta [x y]))))))
 
-(def compass {:up :right
-              :right :down
-              :down :left
-              :left :up})
+(def next-turn {:up :right
+                :right :down
+                :down :left
+                :left :up})
 
 (defn guard-route
   ([tmap start dir]
-   (guard-route [] tmap start dir))
+   (guard-route #{} tmap start dir))
   ([acc tmap start dir]
    (let [walk (guard-walk tmap start dir)
-         stop (last walk)]
+         stop (first (last walk))]
      (if (not (on-map? tmap stop))
-       (concat acc (butlast walk))
-       (recur (concat acc walk) tmap stop (compass dir))))))
+       (into acc (butlast walk))
+       (recur (into acc walk) tmap stop (next-turn dir))))))
 
 (defn guard-route-squares [tmap start dir]
-  (into #{start} (guard-route tmap start dir)))
+  (into #{start} (map first (guard-route tmap start dir))))
+
+(defn guard-route-loop?
+  ([seen tmap start dir]
+   (let [walk (guard-walk tmap start dir)
+         stop (first (last walk))]
+     ;; TODO deal better with the fact that a walk can be zero
+     (cond (nil? stop) (recur seen tmap start (next-turn dir))
+           (not (on-map? tmap stop)) false
+           (some seen walk) true
+           :default (recur (into seen walk) tmap stop (next-turn dir))))))
+
+(defn guard-route-counting-obstacles
+  [tmap start dir]
+  (let [route-squares (into #{} (map first (guard-route tmap start dir)))]
+    (filter #(guard-route-loop? #{} (aoc/tmap-update tmap % \O) start dir) route-squares)))
--- test/aoc_2024/day_6_test.clj
+++ test/aoc_2024/day_6_test.clj
@@ -19,9 +19,9 @@
 
 
 (deftest walking-test
-  (is (= [[4 5] [4 4] [4 3] [4 2] [4 1]] (guard-walk sample-data [4 6] :up))
+  (is (= [[4 5] [4 4] [4 3] [4 2] [4 1]] (map first (guard-walk sample-data [4 6] :up)))
       "Guard should walk until an obstacle occurs")
-  (is (= [[5 6] [6 6] [7 6] [8 6] [9 6] [10 6]] (guard-walk sample-data [4 6] :right))
+  (is (= [[5 6] [6 6] [7 6] [8 6] [9 6] [10 6]] (map first (guard-walk sample-data [4 6] :right)))
       "Guard should walk one space off the edge if there is no obstacle"))
 
 (deftest guard-routing-test
@@ -34,3 +34,25 @@
 
 (deftest part-1-test
   (is (= 4903 (count (guard-route-squares input-data (first (aoc/tmap-find-locations input-data \^)) :up)))))
+
+;; part 2
+
+(deftest loop-test
+  (is (not (guard-route-loop? #{} sample-data [4 6] :up))
+      "The initial sample should not loop")
+  (is (guard-route-loop? #{} (aoc/tmap-update sample-data [3 6] \O) [4 6] :up)
+      "Placing an obstacle to the right of the starting position should induce a loop")
+  (is (guard-route-loop? #{} (aoc/tmap-update sample-data [6 7] \O) [4 6] :up)
+      "Placing an obstacle in the lower right quadrant should induce a loop"))
+
+(deftest obstacle-placing-test
+  (let [obstacles (guard-route-counting-obstacles sample-data [4 6] :up)]
+    (comment 
+      (println "obstacles:" obstacles)
+      (doseq [m (map #(aoc/tmap-update sample-data % \O) obstacles)]
+        (aoc/print-tmap m)
+        (println)))
+    (is (= 6 (count obstacles)))))
+
+(deftest part-2-test
+  (is (= 1911 (count (guard-route-counting-obstacles input-data (first (aoc/tmap-find-locations input-data \^)) :up)))))
