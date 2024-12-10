8ebb4d2622dfc314c97149e7c3e0efac5046a8c8
Joel <joel@gluth.io>
2024-12-10 00:08:15 +0000

    Day 8 parts 1 and 2, quite possibly the worst Clojure code I have every written but I am incredibly tired

---
+++ src/aoc_2024/day_9.clj
@@ -0,0 +1,138 @@
+(ns aoc-2024.day-9
+  (:require [aoc-2024.core :as aoc]
+            [clojure.string :as str]))
+
+(defn parse-fs
+  ([astr]
+   (parse-fs [] 0 :file astr))
+  ([acc current-id mode astr]
+   (if (empty? astr) acc
+       (let [size (parse-long (str (first astr)))
+             mode' (if (= :file mode) :empty :file)
+             id' (if (= :file mode) (inc current-id) current-id)]
+         (if (= 0 size)
+           (recur acc id' mode' (rest astr))
+           (recur (conj acc (if (= :file mode)
+                              {:id current-id :size size}
+                              {:empty :empty :size size}))
+                  id'
+                  mode'
+                  (rest astr)))))))
+
+(defn fs-string
+  ([fs]
+   (fs-string "" fs))
+  ([buf fs]
+   (if (empty? fs)
+     buf
+     (let [obj (first fs)
+           s (:size obj)
+           r (rest fs)]
+       (if (:empty obj)
+         (recur (str buf (apply str (repeat s \.))) r)
+         ;; this will only work with single-digit IDs eh
+         (recur (str buf (apply str (repeat s (:id obj)))) r))))))
+
+
+(defn crush-empty-blocks
+  ([fs]
+   (crush-empty-blocks [] 0 fs))
+  ([acc empty-len fs]
+   (if (empty? fs)
+     (if (= 0 empty-len)
+       acc
+       (conj acc {:empty :empty :size empty-len}))
+     (let [b (first fs)]
+       (if (:empty b)
+         (recur acc (+ empty-len (:size b)) (rest fs))
+         (if (= 0 empty-len)
+           (recur (conj acc b) 0 (rest fs))
+           (recur (into acc [{:empty :empty :size empty-len} b]) 0 (rest fs))))))))
+
+;; THIS IS SUPER DUMB. Actually doing it in steps was dumb
+
+(defn compact-step [fs]
+  (let [compacted (take-while #(not (:empty %)) fs)
+        uncompacted (drop-while #(not (:empty %)) fs)
+        empty-block (first uncompacted)
+        empty-block-size (:size empty-block)
+        last-file (last (filter :id fs))
+        last-file-id (:id last-file)
+        last-file-size (:size last-file)
+        tail (last uncompacted)
+        ;; this is awful, don't code drunk or tired people
+        trail-space (if (:empty tail) tail {:empty :empty :size 0})]
+    (crush-empty-blocks
+     (cond (< empty-block-size last-file-size)
+           ;; move part of the end file inward
+           (concat compacted
+                   [{:id last-file-id :size empty-block-size}]
+                   (take-while #(not (= last-file-id (:id %))) (rest uncompacted))
+                   [{:id last-file-id :size (- last-file-size empty-block-size)} empty-block trail-space])
+           (= empty-block-size last-file-size)
+           ;; swap the end file for the empty block
+           (concat compacted
+                   [last-file]
+                   (take-while #(not (= last-file-id (:id %))) (rest uncompacted))
+                   [empty-block trail-space])
+           :default
+           ;; move the end file inward, consume part of the empty block
+           (concat compacted
+                   [last-file {:empty :empty :size (- empty-block-size last-file-size)}]
+                   (take-while #(not (= last-file-id (:id %))) (rest uncompacted))
+                   [{:empty :empty :size last-file-size} trail-space])
+           ))))
+
+(defn compact-totally
+  ([fs]
+   (compact-totally 0 fs))
+  ([i fs]
+   (if (= 0 (mod i 1000))
+     (println "Compact" i ":" (count (take-while :id fs)) "/" (count fs)))
+   (if (every? :id (butlast fs))
+     fs
+     (recur (inc i) (compact-step fs)))))
+
+(defn checksum
+  ([fs]
+   (checksum 0 0 fs))
+  ([acc idx fs]
+   (if (empty? fs)
+     acc
+     (let [b (first fs)
+           size (:size b)]
+       (if (:empty b)
+         (recur acc (+ idx size) (rest fs))
+         (recur (+ acc
+                   (reduce +
+                           (map #(* (:id b) %)
+                                (range idx (+ idx size)))))
+                (+ idx size)
+                (rest fs)))))))
+
+;; part 2
+
+;; TODO: write a partition-by that only finds the first true return, with before and after
+
+(defn compact-nofrag
+  ([fs]
+   (compact-nofrag fs (apply max (map :id (filter :id fs)))))
+  ([fs id]
+   (if (= 0 (mod id 100))
+     (println "Compact-nofrag:" id))
+   (if (= id 0)
+     fs
+     (let [[left [b] right] (partition-by #(= id (:id %)) fs)
+           b-size (:size b)
+           left' (take-while #(or (:id %) (< (:size %) b-size)) left)
+           right' (drop-while #(or (:id %) (< (:size %) b-size)) left)
+           f (first right')]
+       (if (not f)
+         (recur fs (dec id))
+         (recur (into [] ;; this is there so all the lazy seq take-whiles don't overflow at the end
+                      (concat left'
+                              [b {:empty :empty :size (- (:size f) b-size)}]
+                              (rest right')
+                              [{:empty :empty :size b-size}]
+                              right))
+                (dec id)))))))
---
+++ test/aoc_2024/day_9_test.clj
@@ -0,0 +1,92 @@
+(ns aoc-2024.day-9-test
+  (:require [clojure.test :refer :all]
+            [aoc-2024.day-9 :refer :all]
+            [aoc-2024.core :as aoc]
+            [clojure.string :as str]))
+
+(def sample-data
+  "2333133121414131402")
+
+(def sample-layout
+  "00...111...2...333.44.5555.6666.777.888899")
+
+(def sample-fs
+  [{:id 0 :size 2}
+   {:empty :empty :size 3}
+   {:id 1 :size 3}
+   {:empty :empty :size 3}
+   {:id 2 :size 1}
+   {:empty :empty :size 3}
+   {:id 3 :size 3}
+   {:empty :empty :size 1}
+   {:id 4 :size 2}
+   {:empty :empty :size 1}
+   {:id 5 :size 4}
+   {:empty :empty :size 1}
+   {:id 6 :size 4}
+   {:empty :empty :size 1}
+   {:id 7 :size 3}
+   {:empty :empty :size 1}
+   {:id 8 :size 4}
+   {:id 9 :size 2}])
+
+(deftest parsing-test
+  (is (= sample-fs (parse-fs sample-data))))
+
+(deftest fs-string-test
+  (is (= sample-layout (fs-string sample-fs))))
+
+(deftest crush-empty-blocks-test
+  (let [fs [{:id 0 :size 1}
+            {:empty :empty :size 0}
+            {:id 1 :size 1}
+            {:empty :empty :size 2}
+            {:empty :empty :size 3}
+            {:empty :empty :size 1}
+            {:id 2 :size 1}]]
+    (is (= [{:id 0 :size 1}
+            {:id 1 :size 1}
+            {:empty :empty :size 6}
+            {:id 2 :size 1}]
+           (crush-empty-blocks fs))
+        "consecutive empty blocks should be joined, and zero-length empty blocks removed")))
+
+(deftest compaction-step-test
+  (let [fs (parse-fs "12345")
+        c1 (compact-step fs)
+        c2 (compact-step c1)]
+    (is (= "0..111....22222" (fs-string fs)) "sanity check")
+    (is (= "022111....222.."
+           (fs-string c1)))
+    (is (= "022111222......"
+           (fs-string c2)))))
+
+(deftest total-compaction-test
+  (let [fs (parse-fs "12345")]
+    (is (= "022111222......"
+           (fs-string (compact-totally fs)))))
+  (let [fs (parse-fs sample-data)]
+    (is (= "0099811188827773336446555566.............."
+           (fs-string (compact-totally fs))))))
+
+(deftest checksum-test
+  (is (= 1928 (checksum (compact-totally (parse-fs sample-data))))))
+
+(def input-data
+  (str/trim (slurp "aoc-2024-inputs/input-9.txt")))
+
+(comment 
+  (deftest part-1-test
+    (let [fs (parse-fs input-data)]
+      (is (= 0 (checksum (compact-totally fs)))))))
+
+;; part 2
+
+(deftest compact-nofrag-test
+  (let [fs (parse-fs sample-data)]
+    (is (= "00992111777.44.333....5555.6666.....8888.."
+           (fs-string (compact-nofrag fs))))))
+
+(deftest part-2-test
+  (let [fs (parse-fs input-data)]
+      (is (= 0 (checksum (compact-nofrag fs))))))
