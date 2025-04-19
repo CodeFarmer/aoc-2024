8e9a47a3954f1dcc5fe05c7b5db6cb977c2b3128
Joel <joel@gluth.io>
2025-04-19 22:14:56 +0100

    Day 16 part 2: unit tests passing, just takes forever

--- src/aoc_2024/day_16.clj
+++ src/aoc_2024/day_16.clj
@@ -17,34 +17,56 @@
    :right [:up :down]
    :down  [:right :left]})
 
-(defn possible-moves [tmap [[x y dir] cost]]
+(defn possible-moves [tmap [[x y dir] [pathable cost]]]
   (let [delta (aoc/directions dir)
-        turned (mapv (fn [d] [[x y d] (+ cost 1000)]) (turns dir))
+        turned (mapv (fn [d] [[x y d] [pathable (+ cost 1000)]]) (turns dir))
         pos' (mapv + [x y] delta)]
     (if (= \# (aoc/get-tile tmap pos'))
       turned
-      (conj turned [(conj pos' dir) (inc cost)]))))
+      (conj turned [(conj pos' dir) [(conj pathable pos') (inc cost)]]))))
 
 (defn -walk [tmap finish q seen]
-  (if (empty? q) 0
-       (let [[state cost] (peek q)
-             loc (take 2 state)
-             useful? (fn [[s c]] (< c (get seen s Integer/MAX_VALUE)))]
-         (if (= finish loc)
-           cost
-           (recur tmap
-                  finish
-                  (into (pop q) (filter useful? (possible-moves tmap [state cost])))
-                  (assoc seen state cost))))))
+  (if (empty? q) [#{} 0] ; technically this is a bug
+      (let [[state [pathable cost]] (peek q)
+            loc (take 2 state)
+            useful? (fn [[s [p c]]] (<= c (second (get seen s [#{} Integer/MAX_VALUE]))))
+            finished-states (filter #(= finish (take 2 %)) (keys seen))
+            finished-state (first finished-states)]
+        (if finished-state ;; wait do we even care?
+          (let [[finished-pathable finished-cost] (seen finished-state)]
+            (if (> cost finished-cost) ; we are done
+              [(reduce into #{}
+                       (->> (select-keys seen finished-states)
+                            (map second)
+                            (map first)))
+               finished-cost]
+              (recur tmap
+                       finish
+                       (pop q)
+                       (assoc seen state [(into (get-in seen [state 0] #{}) pathable) cost]))))
+          (recur tmap
+                 finish
+                 (into (pop q) (filter useful? (possible-moves tmap [state [pathable cost]])))
+                 (assoc seen state [(into (get-in seen [state 0] #{}) pathable) cost]))))))
 
 (defn best-path-score
   ([tmap]
    (best-path-score tmap
                     (finish-square tmap)
-                    (conj (pq/priority-queue #(- (second %))) [(starting-state tmap) 0])
+                    (conj (pq/priority-queue (fn [[_ [_ c]]] (- c))) [(starting-state tmap) [#{} 0]])
                     {}))
   ([tmap finish q seen]
-   (-walk tmap finish q seen)))
+   (let [[pathable cost] (-walk tmap finish q seen)]
+     cost)))
 
-(defn count-best-path-tiles [tmap]
-  0)
+(defn count-best-path-tiles
+  ([tmap]
+   (count-best-path-tiles tmap
+                          (finish-square tmap)
+                          (conj (pq/priority-queue (fn [[_ [_ c]]] (- c))) [(starting-state tmap) [#{} 0]])
+                          {}))
+  ([tmap finish q seen]
+   (let [[pathable cost] (-walk tmap finish q seen)]
+
+     ;; (aoc/print-tmap (reduce (fn [a pos] (aoc/tmap-update a pos \O)) tmap pathable))
+     (inc (count pathable)))))
--- test/aoc_2024/day_16_test.clj
+++ test/aoc_2024/day_16_test.clj
@@ -29,15 +29,15 @@
   (is (= [13 1] (finish-square example-map))))
 
 (deftest possible-moves-test
-  (is (= [[[1 13 :up] 1000]
-          [[1 13 :down] 1000]
-          [[2 13 :right] 1]]
-         (possible-moves example-map [(starting-state example-map) 0]))
+  (is (= [[[1 13 :up] [#{} 1000]]
+          [[1 13 :down] [#{} 1000]]
+          [[2 13 :right] [#{[2 13]} 1]]]
+         (possible-moves example-map [(starting-state example-map) [#{} 0]]))
       "Possible moves from the start are turn 90 degrees left or right, or step forward, with appropriate costs")
 
-  (is (= [[[3 13 :up] 1003]
-          [[3 13 :down] 1003]]
-         (possible-moves example-map [[3 13 :right] 3]))
+  (is (= [[[3 13 :up] [#{} 1003]]
+          [[3 13 :down] [#{} 1003]]]
+         (possible-moves example-map [[3 13 :right] [#{} 3]]))
       "Possible moves are turn 90 degrees left or right, where stepping forward is not possible"))
 
 
@@ -67,8 +67,25 @@
 (def input-map
   (aoc/tmap (slurp "aoc-2024-inputs/input-16.txt")))
 
-(deftest part-1-test
-  (is (= 65436 (best-path-score input-map))))
+(comment 
+  (deftest part-1-test
+    (is (= 65436 (best-path-score input-map)))))
+
+;; part 2
+
+(comment 
+  (deftest bug-hunting-test
+    (let [buggy-map
+          (aoc/tmap "######
+######
+###E.#
+#...##
+#.#.##
+#....#
+#.####
+#S..##
+######")]
+      (is (= 11 (count-best-path-tiles buggy-map))))))
 
 (deftest path-counting-test
   (is (= 45 (count-best-path-tiles example-map)))
